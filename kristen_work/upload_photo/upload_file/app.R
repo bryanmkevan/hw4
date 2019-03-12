960074076cbb891be4680270b3baae48a9b1ba82
Kristen McGreevy <kristenmae@g.ucla.edu>
2019-03-12 02:27:26 +0000

    Upload image and display it and the file info. needs to change resolution / size of display

--- kristen_work/upload_photo/upload_file/app.R
+++ kristen_work/upload_photo/upload_file/app.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 # Define UI for data upload app ----
-ui <- fluidPage(
+shinyUI(fluidPage(
   
   # App title ----
   titlePanel("Uploading Files"),
@@ -13,77 +13,47 @@ ui <- fluidPage(
     sidebarPanel(
       
       # Input: Select a file ----
-      fileInput("file1", "Choose an image File",
-                multiple = TRUE,
-                accept = c('image/png', 'image/jpeg')),
-      
+            fileInput(inputId = 'files', 
+                      label = 'Select an Image',
+                      multiple = TRUE,
+                      accept=c('image/png', 'image/jpeg'))),
+
       # Horizontal line ----
-      tags$hr(),
-      
-      # Input: Checkbox if file has header ----
-      checkboxInput("header", "Header", TRUE),
-      
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
-      
-      # Input: Select quotes ----
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
-      
-      # Horizontal line ----
-      tags$hr(),
-      
-      # Input: Select number of rows to display ----
-      radioButtons("disp", "Display",
-                   choices = c(Head = "head",
-                               All = "all"),
-                   selected = "head")
-      
+      tags$hr()
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
-      
-      # Output: Data file ----
-      tableOutput("contents")
-      
+      tableOutput('files'),
+      uiOutput('images')
     )
     
   )
 )
 
+
+
 # Define server logic to read selected file ----
-server <- function(input, output) {
-  
-  output$contents <- renderTable({
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-    
-    req(input$file1)
-    
-    df <- read.csv(input$file1$datapath,
-                   header = input$header,
-                   sep = input$sep,
-                   quote = input$quote)
-    
-    if(input$disp == "head") {
-      return(head(df))
-    }
-    else {
-      return(df)
-    }
-    
-  })
-  
+  server <- function(input, output) {
+# 
+#   output$file2 <- renderImage({
+#     
+#     # Generate the image file
+#     png(file1, width = 100, height = 100,
+#         res = 72)
+     imageOutput("file1")
+#     })
+#   
+#   output$preImage <- renderImage({
+#     # When input$n is 3, filename is ./images/image3.jpeg
+#     filename <- normalizePath(file.path('/home/kristenmae/biostat-m280-2019-winter/hw4/kristen_work/cookies/images',
+#                                         paste('10', '.jpeg', sep='')))
+#     
+#   }, deleteFile = FALSE)
+#   
+#   
+#   
+#   
 }
 # Run the app ----
 shinyApp(ui, server)
